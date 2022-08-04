abf38c990194b7b3564bbc14df4dd4068e1a1cbe
continue-revolution <3357667574@qq.com>
2022-08-04 15:38:15 +0800

    add a small experiment

--- run_exp.sh
+++ run_exp.sh
@@ -3,6 +3,16 @@
 if [ "$1" == "--cloud" ]; then
     source install.sh
 fi
-python experiment.py --iter=1000 > out 2> err
-rm err
-python parse.py > output
+
+if [ "$2" == "--expe" ]; then
+    python experiment.py --iter=1000 > out 2> err
+    rm err
+    python parse.py > output
+fi
+
+if [ "$2" == "--idx" ]; then
+    for i in 1 2 4 8 16 32
+    do
+        OMP_NUM_THREADS=$i python index_select.py
+    done
+fi
