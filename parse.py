a353a9a3adadf2bba71333cafb195b98829d5faf
continue-revolution <3357667574@qq.com>
2022-08-05 12:02:02 +0800

    modify parse

--- parse.py
+++ parse.py
@@ -1,21 +1,25 @@
-from distutils.command.build_scripts import first_line_re
 import pathlib
 from absl import app, flags
-flags.DEFINE_string('filename', 'out', 'filename of output')
+# flags.DEFINE_string('filename', 'out', 'filename of output')
 
 def main(_):
-    filename = flags.FLAGS.filename
-    file = pathlib.Path(filename)
-    contents = file.read_text()
-    lines = contents.split('\n')
+    lines = []
+    for i in [1, 2, 4, 8, 16, 32]:
+        contents = pathlib.Path(f'out{i}').read_text().split('\n')[1:-1]
+        contents = map(lambda x: '\t'.join([x.split('\t')[0] + f'-{i}', x.split('\t')[1]]), contents)
+        lines.extend(contents)
+    # filename = flags.FLAGS.filename
+    # file = pathlib.Path(filename)
+    # contents = file.read_text()
+    # lines = contents.split('\n')
     datas = {}
-    first_line = True
+    # first_line = True
     for line in lines:
-        if first_line:
-            first_line = False
-            continue
+        # if first_line:
+        #     first_line = False
+        #     continue
         if line:
-            category, data, _ = line.split('\t')
+            category, data = line.split('\t')
             key = category.split('-')[0]
             if key not in datas:
                 datas[key] = [(category, float(data))]
