e3a3fd82fc96a36bf72e5c83fa582e36ca5c8d3e
continue-revolution <3357667574@qq.com>
2022-08-05 12:03:20 +0800

    modify shell

--- run_exp.sh
+++ run_exp.sh
@@ -14,6 +14,7 @@ if [ "$2" == "--idx" ]; then
     for i in 1 2 4 8 16 32
     do
         OMP_NUM_THREADS=$i python experiment.py --iter=1000 > out$i 2> err
-        rm err
     done
+    rm err
+    python parse.py > output$3
 fi
